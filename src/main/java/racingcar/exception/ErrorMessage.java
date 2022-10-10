c1032a88c71f63b4e2a88e8a186e2fb03ef1964d
kkwan0226 <kkwan0226@gmail.com>
2022-10-10 17:22:11 +0900

    feat($Round): 레이싱 게임 라운드 객체 및 테스트 구현

--- src/main/java/racingcar/exception/ErrorMessage.java
+++ src/main/java/racingcar/exception/ErrorMessage.java
@@ -5,7 +5,7 @@ public enum ErrorMessage {
     INVALID_CAR_NAME_LENGTH("Check the length of the car name"),
     INVALID_CAR_NAME_EMPTY("The car name cannot be empty"),
     INVALID_CAR_LIST_SIZE("Please check the number of cars for the racing game"),
-    ;
+    INVALID_INPUT_ROUND("Check the input value or type");
 
     private final String errorMessage;
 
